4a57dcbc3dfe94c3eb95c6c967b996e21b6c226f
dylandettloff <dylandettloff@gmail.com>
2026-06-09 20:40:21 -0500

    Allow app to read press stories

--- bbs-backend/src/api/press/routes/press.ts
+++ bbs-backend/src/api/press/routes/press.ts
@@ -4,4 +4,9 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::press.press');
+export default factories.createCoreRouter('api::press.press', {
+  config: {
+    find: { auth: false },
+    findOne: { auth: false },
+  },
+});
